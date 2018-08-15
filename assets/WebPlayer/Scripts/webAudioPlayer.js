64b5ea72f24bc23205d4b1567495577d960bf17e
ZanifSandy <zanif.sandy@student.mec.cuny.edu>
2018-08-14 23:11:21 -0400

    updates

--- assets/WebPlayer/Scripts/webAudioPlayer.js
+++ assets/WebPlayer/Scripts/webAudioPlayer.js
@@ -3,30 +3,35 @@ var prevbutton = document.getElementById("prev");
 var fowardbutton = document.getElementById("foward");
 //var display = document.getElementById("Display");
 
-var songList = ['pink.wav', 'jordy.mp3']
+var songList = ['assets/MusicLibrary/Music/pink.wav', 'assets/MusicLibrary/Music/jordy.mp3'];
+var songTitle = ['Pink', 'Jordy']
+var pictureList = ['assets/MusicLibrary/Title-Images/pink.jpeg','assets/MusicLibrary/Title-Images/jordy.jpeg'];
 var position = 0;
 var isPlay =false;
 
 
 function display()
 {
-document.getElementById("Display").innerHTML = ` ${songList[position]}`;
+document.getElementById("Display").innerHTML = `    <div id="song-title">${songTitle[position]}
+                                                      <p><div id="song-artist">Zayslash</div></p>
+                                                           </div>
+                                                    <img id="Display-img" src=${pictureList[position]}>`;
 
+//document.getElementById("Display").innerHTML = `<img id="Display-img" src="jordy.jpeg">`
+// document.getElementById("Display").innerHTML = `<div id="song-title">title</div>`
 }
 
 function togglePlay(song){
     if (isPlay){
       audio.pause()
       isPlay = false
-      document.getElementById("play-img").src= "play.png";
+      document.getElementById("play-img").src= "assets/webPlayer/Images/play.png";
     }else{
       isPlay =true;
-      document.getElementById("play-img").src = "pause.png";
+      document.getElementById("play-img").src = "assets/webPlayer/Images/pause.png";
       document.getElementById("play-img").style.width = "50px";
       playsound(song);
-
     }
-
 }
 
 function playsound(song){
---
+++ main.py
@@ -0,0 +1,21 @@
+import webapp2
+import os
+import jinja2
+
+#remember, you can get this by searching for jinja2 google app engine
+jinja_current_dir = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class WebStart(webapp2.RequestHandler):
+    def get(self):
+        start_template = jinja_current_dir.get_template("index.html")
+        self.response.write(start_template.render())
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', WebStart)
+], debug=True)
